2f83c268459ba82b933f648070d3226e8079f862
Kishore G <166317432+Mr-Kishore@users.noreply.github.com>
2025-06-10 10:25:24 +0530

    Update AverageFinder.java

--- practice/src/com/myjava/practice/basics/AverageFinder.java
+++ practice/src/com/myjava/practice/basics/AverageFinder.java
@@ -13,28 +13,28 @@ public class AverageFinder {
         String deviceId = scanner.nextLine();
 
         System.out.print("Enter temperature 1: ");
-        int t1 = scanner.nextInt();
+        float t1 = scanner.nextInt();
 
         System.out.print("Enter temperature 2: ");
-        int t2 = scanner.nextInt();
+        float t2 = scanner.nextInt();
 
         System.out.print("Enter temperature 3: ");
-        int t3 = scanner.nextInt();
+        float t3 = scanner.nextInt();
 
         System.out.print("Enter temperature 4: ");
-        int t4 = scanner.nextInt();
+        float t4 = scanner.nextInt();
 
         System.out.print("Enter temperature 5: ");
-        int t5 = scanner.nextInt();
+        float t5 = scanner.nextInt();
 
-        int sum = t1 + t2 + t3 + t4 + t5;
-        int average = sum / 5;
+        float sum = t1 + t2 + t3 + t4 + t5;
+        float average = sum / 5;
 
         System.out.println("The average temperature of "
                 + deviceId + " is = " + average);
 
         scanner.close();
     }
-    }
+}
 
 
