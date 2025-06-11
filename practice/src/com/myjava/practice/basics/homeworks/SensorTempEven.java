ad011a7582ac236ee1f1ff514ceca0e023307fb0
Kishore <kishoreprogramminghub@gmail.com>
2025-06-11 12:54:30 +0530

    homeworks doneeeeee

--- practice/src/com/myjava/practice/basics/homeworks/SensorTempEven.java
+++ practice/src/com/myjava/practice/basics/homeworks/SensorTempEven.java
@@ -8,13 +8,13 @@ public class SensorTempEven
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter number of sensors: ");
-        int n = scanner.nextInt();
+        int numOfSensors = scanner.nextInt();
 
-        int[] sensorIds = new int[n];
-        double[] temperatures = new double[n];
+        int[] sensorIds = new int[numOfSensors];
+        double[] temperatures = new double[numOfSensors];
 
         // Input
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < numOfSensors; i++) {
             System.out.print("Enter Sensor ID for sensor " + (i + 1) + ": ");
             sensorIds[i] = scanner.nextInt();
 
@@ -23,7 +23,7 @@ public class SensorTempEven
         }
 
         System.out.println("\nEven Temperatures:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < numOfSensors; i++) {
             if (temperatures[i] % 2 == 0) {
                 System.out.println("Sensor " + sensorIds[i] + ", temp " + temperatures[i]);
             }
