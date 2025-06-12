c4bb82ec937369ef24cbbfd31571e78eba9d95cf
Kishore <kishoreprogramminghub@gmail.com>
2025-06-12 16:22:18 +0530

    new project

---
+++ practice/src/com/tempdata/beans/Building.java
@@ -0,0 +1,12 @@
+package com.tempdata.beans;
+
+public class Building {
+    private Floor[] floors;
+    private String color;
+    private String shape;
+    private String name;
+    private String noOfFloors;
+    private float height;
+    private int squareFeet;
+    private boolean parking;
+}
---
+++ practice/src/com/tempdata/beans/Sensor.java
@@ -0,0 +1,10 @@
+package com.tempdata.beans;
+
+public class Sensor {
+    private TempData[] tempData;
+    private String sensorType;
+    private String sensorID;
+    private String sensorName;
+    private String brand;
+    private String status;
+}
